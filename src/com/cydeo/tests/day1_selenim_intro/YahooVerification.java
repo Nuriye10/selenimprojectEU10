8643e0dd744822a1f5cc85bed5dfcd04464068bb
Nuriye kader <nadirnurani@gmail.com>
2023-01-22 00:19:39 +0100

    day1 basic add

---
+++ src/com/cydeo/tests/day1_selenim_intro/YahooVerification.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests.day1_selenim_intro;
+
+public class YahooVerification {
+    public static void main(String[] args) {
+
+      //  TC #1, yahoo title verification
+        //TC 2.Open Chrome browser
+        //3. Verify title
+        //Expected ; Yahoo.
+
+
+    }
+
+}
