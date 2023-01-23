47d86be91b61cc2c87768a1b4e1b403fda0fff66
Nuriye kader <nadirnurani@gmail.com>
2023-01-23 23:44:42 +0100

    day2 Task2_LinkText_Practice,Added

--- src/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkTextPractice.java
+++ src/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkTextPractice.java
@@ -46,7 +46,19 @@ public class T2_LinkTextPractice {
             System.out.println("Title verification FAILED!!!");
         }
         //5.Go back to home page by using the .back();
+
+        driver.navigate().back();
+
         //6.Verify title equals:
         // Expected :Practice
+         expectedTitle ="Practice";
+         actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASS!");
+        }else {
+            System.out.println("Title verification FAILED!!!");
+        }
+
     }
 }
