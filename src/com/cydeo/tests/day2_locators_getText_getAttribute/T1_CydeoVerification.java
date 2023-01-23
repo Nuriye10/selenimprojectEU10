1f4b57189b7433c079d72bde0e57d5ef8aa8723f
Nuriye kader <nadirnurani@gmail.com>
2023-01-23 21:58:38 +0100

    day2_Task1_CydeoVerification

--- src/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerification.java
+++ src/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerification.java
@@ -1,13 +1,38 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerification {
 
     public static void main(String[] args) {
     //TC,1: Cydeo practice tool verification
     //1, Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+
+
+
+
     //2.Go to https://practice.cydeo.com
+        driver.navigate().to( "https://practice.cydeo.com");
+
     //3.Verify URL contains
+      String expectedURL ="cydeo";
+      String actualURL = driver.getCurrentUrl();
+      if(actualURL.contains(expectedURL)){
+          System.out.println("URL verification PASSED!!!");
+      }else{
+          System.out.println("URL verification FAILED!!!");
+      }
+
     //Expected:cydeo
+
+
     //4.Verify title:
     //Expected practice
 
