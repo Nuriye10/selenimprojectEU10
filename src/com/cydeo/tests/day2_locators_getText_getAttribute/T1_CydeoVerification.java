6bed745137a5b4a0b81e498a545379d1b878e79e
Nuriye kader <nadirnurani@gmail.com>
2023-01-23 21:44:53 +0100

    day2_Task1_CydeoVerification

---
+++ src/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerification.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T1_CydeoVerification {
+
+    public static void main(String[] args) {
+    //TC,1: Cydeo practice tool verification
+    //1, Open Chrome browser
+    //2.Go to https://practice.cydeo.com
+    //3.Verify URL contains
+    //Expected:cydeo
+    //4.Verify title:
+    //Expected practice
+
+
+
+
+
+
+
+
+    }
+}
