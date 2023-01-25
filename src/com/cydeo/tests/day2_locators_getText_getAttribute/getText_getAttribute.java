3a7c4129593d10ab3245e241a60a7332761a0047
Nuriye kader <nadirnurani@gmail.com>
2023-01-25 23:23:26 +0100

    Task4_LibraryLogInPage created and completed.pushed.

---
+++ src/com/cydeo/tests/day2_locators_getText_getAttribute/getText_getAttribute.java
@@ -0,0 +1,36 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class getText_getAttribute {
+    public static void main(String[] args) {
+
+        // 1.open Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+       //2.go to :https://practice.cydeo.com/registration_form
+       driver.get("https://practice.cydeo.com/registration_form");
+
+       //3.Verify header text is as expected:
+        WebElement headerText = driver.findElement(By.tagName("h2"));
+
+       String expectedHeaderText="Registration form";
+
+       String actualHeaderText=headerText.getText();
+
+       if(actualHeaderText.equals(expectedHeaderText)){
+           System.out.println("Header text verification PASSED!");
+       }else{
+           System.out.println("Header text verification FAILED!!!");
+       }
+    }
+
+
+}
