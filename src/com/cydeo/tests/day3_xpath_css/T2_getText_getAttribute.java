6423d9a35390c7c3511c2bd7806141b3eddbbda7
Nuriye kader <nadirnurani@gmail.com>
2023-01-28 22:00:14 +0100

    getAttribute add step

--- src/com/cydeo/tests/day3_xpath_css/T2_getText_getAttribute.java
+++ src/com/cydeo/tests/day3_xpath_css/T2_getText_getAttribute.java
@@ -20,10 +20,28 @@ public class T2_getText_getAttribute {
         driver.get("https://login.nextbasecrm.com/");
         //3, Verify "remember me" label text is expected:
         WebElement rememberMeLabel = driver.findElement(By.className("login-item-checkbox-label"));
+        String expectedRememberMeLabel= "Remember me on this computer";
+        String actualRememberMeLabel = rememberMeLabel.getText();
+
+        if(actualRememberMeLabel.equals(expectedRememberMeLabel)){
+            System.out.println("Label verification PASSED!");
+
+        }else{
+            System.out.println("Label verification FAILED!!!");
+        }
 
 
         //Expected:Remember me on this computer
         //4, Verify "Forgot password " link text is expected:
+        WebElement forgotPassWordLink = driver.findElement(By.className("login-link-forgot-pass"));
+        String expectedForgotPassWordLinkText="Forgot your password?";
+        String actualForgotPassWordLinkText= forgotPassWordLink.getText();
+        if(actualForgotPassWordLinkText.equals(expectedForgotPassWordLinkText)){
+            System.out.println("Forgot password verification PASSED!");
+        }else{
+            System.out.println("Forgot password verification FAILED!!!");
+        }
+
         //5, Verify "forgot password"href attributes  value contains expected:
         //Expected:forgot_password=yes
 
