7523875da7e9c82a35833acf3836e22990e7f6c8
Nuriye kader <nadirnurani@gmail.com>
2023-02-19 17:44:26 +0100

    day4, check boxes Locate the checkboxes

--- src/Day4_findElement_checkBox/T5_CheckBoxes.java
+++ src/Day4_findElement_checkBox/T5_CheckBoxes.java
@@ -1,7 +1,9 @@
 package Day4_findElement_checkBox;
 
 import com.cydeo.utilities.webDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +16,11 @@ public class T5_CheckBoxes {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://practice.cydeo.com/checkboxes");
+
+        //we have tp locate checkboxes
+        WebElement checkbox1 = driver.findElement(By.xpath(""));
+        WebElement checkbox2 = driver.findElement(By.xpath(""));
+
 
     }
 
