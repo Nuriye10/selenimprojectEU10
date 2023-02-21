7fef1828e8070f3802fb928b7ae9c9468a68a0ea
Nuriye kader <nadirnurani@gmail.com>
2023-02-21 21:00:42 +0100

    TestNG_intro1

---
+++ src/com/cydeo/test/day5_testNG_intro_dropdown/TestNG_intro1.java
@@ -0,0 +1,13 @@
+package com.cydeo.test.day5_testNG_intro_dropdown;
+
+import org.testng.annotations.Test;
+
+public class TestNG_intro1 {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running...");
+
+
+    }
+}
