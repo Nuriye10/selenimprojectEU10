9c0bf4fe7fc2e3924124c157b7346070ec9df764
Nuriye kader <nadirnurani@gmail.com>
2023-02-21 21:39:50 +0100

    TestNG_intro1 AfterMethod

--- src/com/cydeo/test/day5_testNG_intro_dropdown/TestNG_intro1.java
+++ src/com/cydeo/test/day5_testNG_intro_dropdown/TestNG_intro1.java
@@ -1,8 +1,21 @@
 package com.cydeo.test.day5_testNG_intro_dropdown;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_intro1 {
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        System.out.println("--->BeforeMethod is running!");
+    }
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---->AfterMethod is running!");
+
+    }
 
     @Test
     public void test1(){
@@ -10,4 +23,12 @@ public class TestNG_intro1 {
 
 
     }
+     @Test
+    public void test2(){
+
+        System.out.println("Test 2 is running....");
+    }
+
+
+
 }
