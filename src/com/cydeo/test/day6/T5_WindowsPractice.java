96e637f7f6f71d2c0710df5ae8d04004da09d53c
Nuriye kader <nadirnurani@gmail.com>
2023-02-27 21:53:26 +0100

    day6 window_task_test passed

--- src/com/cydeo/test/day6/T5_WindowsPractice.java
+++ src/com/cydeo/test/day6/T5_WindowsPractice.java
@@ -1,7 +1,10 @@
 package com.cydeo.test.day6;
 
 import com.cydeo.utilities.webDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,14 +23,44 @@ public class T5_WindowsPractice {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        //2.Go to website: "https://practice.cydeo.com/iframe
-        driver.get("https://practice.cydeo.com/iframe");
+        //2.Go to website: "https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
 
 
     }
 
     @Test
     public void multiple_Window_Test(){
+
+        //Storing the main pages window handle as string is
+        //good practice for future re-usable purposes
+        String mainHandle = driver.getWindowHandle();
+
+        System.out.println( "mainHandle =" + mainHandle);
+
+        //4. Assert: Title is " windows"
+
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
+        //5. Switch to new window.
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+        actualTitle = driver.getTitle();
+        System.out.println("Title after click: " + actualTitle);
+
+        //6.Assert : Title is " New Window"
+
+        for(String each: driver.getWindowHandles()){
+
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching the window:"+ driver.getTitle());
+        }
+
+
+
+
+
 
 
 
