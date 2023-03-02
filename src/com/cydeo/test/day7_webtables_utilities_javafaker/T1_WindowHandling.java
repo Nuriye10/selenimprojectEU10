2943ee6d3a18021d9252c3da28b6e7732cd2ebbd
Nuriye kader <nadirnurani@gmail.com>
2023-03-02 02:18:28 +0100

    day7_JavaFaker.Test pass.

---
+++ src/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -0,0 +1,2 @@
+package com.cydeo.test.day7_webtables_utilities_javafaker;public class T1_WindowHandling {
+}
--- src/com/cydeo/test/day7_webtables_utilities_javafaker/T1_windowHandling.java
+++ src/com/cydeo/test/day7_webtables_utilities_javafaker/T1_windowHandling.java
@@ -1,50 +1,40 @@
 package com.cydeo.test.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.webDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class T1_windowHandling {
+public class T1_WindowHandling {
 
     public WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
-
         webDriverFactory WebDriverFactory = null;
-        driver= WebDriverFactory.getDriver("chrome");
+
+
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
     }
 
-    @Test
-    public void window_handling_test(){
-        //2, Goto: https://www.amazon.com
-        driver.get(" https://www.amazon.com");
-
-        //3.Copy-past the lines from below into your class
-
-        ((JavaScriptExecutor) driver).executeScript("window.open(https://google.com','_blank');\");");
-        ((JavaScriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
-        ((JavaScriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
 
+  @Test
+  public void window_handling_test(){
+        driver.get("https://www.amazon.com");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://google.com','_blank');");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://etsy.com','_blank');");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://facebook.com','_blank');");
 
 
+  }
 
 
-        //4. Create a logic to switch to the tab where Etsy.com is open
-        //5.Assert : Title contains "Etsy"
-        //Lines to ba pasted:
-
-
-
-
-
-    }
 }
