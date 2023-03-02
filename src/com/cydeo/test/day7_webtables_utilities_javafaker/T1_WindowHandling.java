8d58f6dbdaa3ec1766fc153e098c7fe0cf7840c5
Nuriye kader <nadirnurani@gmail.com>
2023-03-02 08:42:45 +0100

    day7_JavaFaker.Test pass_Current URL

--- src/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,2 +1,58 @@
-package com.cydeo.test.day7_webtables_utilities_javafaker;public class T1_WindowHandling {
-}
+package com.cydeo.test.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.webDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        webDriverFactory WebDriverFactory = null;
+
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+    }
+
+
+
+  @Test
+  public void window_handling_test(){
+        driver.get("https://www.amazon.com");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://google.com','_blank');");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://etsy.com','_blank');");
+      ((JavascriptExecutor)driver).executeScript("window.open('https://facebook.com','_blank');");
+
+
+      Set<String>allWindowHandles = driver.getWindowHandles();
+      for (String each : allWindowHandles) {
+          driver.switchTo().window(each);
+
+
+          System.out.println("Current URL:" + driver.getCurrentUrl());
+      }
+
+
+
+      }
+
+
+      }
+
+
+
+
+
+
+
