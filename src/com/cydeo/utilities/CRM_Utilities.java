382d6ed85c313f9cc538a0677fb13fd2699cf32e
Nuriye kader <nadirnurani@gmail.com>
2023-03-04 23:32:19 +0100

    Utils test _Day7 code passed

--- src/com/cydeo/utilities/CRM_Utilities.java
+++ src/com/cydeo/utilities/CRM_Utilities.java
@@ -17,15 +17,29 @@ public class CRM_Utilities {
         loginButton.click();
 
 
-
-
-
-
-
-
-
-
     }
+    public static void crm_login(WebDriver driver ,String username,String password){
+
+    WebElement inputUsername= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys(username);
+
+
+
+
+    WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("password");
+
+    WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
+}
+
+
+
+
+
+
 
 
 
